ac4e0de73cec1074383c9370be7882f4e6277d4c
Samet Kum <kumsamet@gmail.com>
2025-03-01 00:05:57 +0300

    feat(outputs): add lambda function ARN and invoke ARN outputs to lambda module

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -2,3 +2,13 @@ output "lambda_function_name" {
   description = "The name of the Lambda function"
   value       = aws_lambda_function.this.function_name
 }
+
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.this.arn
+}
+
+output "lambda_function_invoke_arn" {
+  description = "The invoke ARN of the Lambda function"
+  value       = aws_lambda_function.this.invoke_arn
+}
