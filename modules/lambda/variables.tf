c0bd5a6a6fc2ff5fdddf9bc08c34c02cb7702241
Samet Kum <kumsamet@gmail.com>
2025-02-28 23:44:14 +0300

    docs(variables): add examples for variables

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,25 +1,30 @@
 variable "function_name" {
   description = "The name of the Lambda function"
   type        = string
+  // Example: "my_lambda_function"
 }
 
 variable "handler" {
   description = "The handler for the Lambda function"
   type        = string
+  // Example: "index.handler"
 }
 
 variable "runtime" {
   description = "The runtime for the Lambda function"
   type        = string
+  // Example: "nodejs14.x"
 }
 
 variable "source_dir" {
   description = "The source directory of the Lambda function code"
   type        = string
+  // Example: "./src"
 }
 
 variable "environment_variables" {
   description = "Environment variables for the Lambda function"
   type        = map(string)
   default     = {}
+  // Example: { "ENV_VAR1" = "value1", "ENV_VAR2" = "value2" }
 }
