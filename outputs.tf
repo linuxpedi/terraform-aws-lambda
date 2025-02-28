7662195897a420488fa96dae539056c2560815e6
Samet Kum <kumsamet@gmail.com>
2025-02-28 23:49:45 +0300

    feat(outputs): add outputs for ARN, and invoke ARN

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "lambda_function_name" {
   description = "The name of the Lambda function"
   value       = module.linuxpedi.lambda_function_name
 }
+
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = module.linuxpedi.lambda_function_arn
+}
+
+output "lambda_function_invoke_arn" {
+  description = "The invoke ARN of the Lambda function"
+  value       = module.linuxpedi.lambda_function_invoke_arn
+}
